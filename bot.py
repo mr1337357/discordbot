d9bfc81bbd345f132400dc52400e1b0a3255faf3
mr1337357 <mr1337357@github.com>
2022-09-20 18:18:41 +0000

    add a couple new commands

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import discord
 from dispatcher import dispatcher
 import random
+import sys
 
 d = dispatcher()
 d.register_builtin('!ping', lambda msg: msg.channel.send('pong'))
 d.register_builtin('!source', lambda msg: msg.channel.send('https://github.com/mr1337357/discordbot'))
+d.register_builtin('!log',lambda msg: print(msg))
+d.register_builtin('!reload',lambda msg: sys.exit(0))
+
 d.register_cmd('!roll', lambda msg: msg.channel.send(random.randint(1,20)))
+d.register_cmd('!socks', lambda msg: msg.channel.send(random.choice(['UwU','OwO','onii-chan'])))
 
 class MyClient(discord.Client):
     async def on_ready(self):
