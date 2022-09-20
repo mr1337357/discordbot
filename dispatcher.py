223288c0940fef9f593a848a7ceaffda7417243d
mr1337357 <miss@papaya>
2022-09-20 11:30:37 -0700

    add permission system

--- dispatcher.py
+++ dispatcher.py
@@ -5,24 +5,29 @@ class dispatcher:
         self.cmds = {}
         self.default = lambda x: x
 
-    def register_builtin(self,match,cmd):
+    def register_builtin(self,match,handler):
         m = re.compile(match)
-        self.builtins[m] = cmd
+        self.builtins[m] = (handler,None,None)
 
-    def register_cmd(self,match,cmd):
+    def register_cmd(self,match,handler,role=None,channel=None):
         m = re.compile(match)
-        self.builtins[m] = cmd
+        self.builtins[m] = (handler,role,channel)
 
-    def register_default(self,cmd):
-        self.default = cmd
+    def register_default(self,handler):
+        self.default = handler
+
+    def check_cmd_permission(self,message,handler):
+        print(message)
+        return True
 
     async def dispatch_cmd(self,message):
         for cmd,handler in self.builtins.items():
             if cmd.match(message.content):
-                await handler(message)
+                await handler[0](message)
                 return
         for cmd,handler in self.cmds.items():
             if cmd.match(message.content):
-                await handler(message)
-                return
+                if check_cmd_permission(message,handler):
+                    await handler[0](message)
+                    return
         self.default(message)
